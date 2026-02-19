e1fbbd7fd82ca0dfd1ac7a540d34d91b5cc9e0e0
lucas-matricarde <lucas.eduardo@wises.com.br>
2026-02-19 02:43:59 -0300

    Começo main-section

---
+++ src/app/shared/main-section/main-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainImg } from "../main-img/main-img";
+
+@Component({
+  selector: 'main-section',
+  imports: [MainImg],
+  templateUrl: './main-section.html',
+  styleUrl: './main-section.css',
+})
+export class MainSection {
+
+}
